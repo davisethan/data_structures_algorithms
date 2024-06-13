6cc603790f78afe74b7722297415efee9c0be0f0
Ethan Davis <ethandavisecd@gmail.com>
2024-06-12 18:18:32 -0700

    Updated house robber 2 algorithm

--- algorithms/dynamic_programming/house_robber_2/solution.py
+++ algorithms/dynamic_programming/house_robber_2/solution.py
@@ -25,9 +25,9 @@ class Solution:
         memo = [0 if k == 0 else None for k in range(n + 1)]
 
         for k in range(1, n + 1):
-            if 0 <= k - 2:
-                memo[k] = max(money[k - 1] + memo[k - 2], memo[k - 1])
-            else:
+            if k - 2 < 0:
                 memo[k] = max(money[k - 1], memo[k - 1])
+            else:
+                memo[k] = max(money[k - 1] + memo[k - 2], memo[k - 1])
 
         return memo[n]
