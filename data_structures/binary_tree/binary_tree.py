0567d0539e4d3ef1088e685991af2fabf65aa579
Ethan Davis <ethandavisecd@gmail.com>
2024-06-24 11:05:34 -0700

    Updated binary tree node data structure

--- data_structures/binary_tree/binary_tree.py
+++ data_structures/binary_tree/binary_tree.py
@@ -3,3 +3,15 @@ class BinaryTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def __repr__(self):
+        values = []
+        self.dfs(self, values)
+        return str(values)
+
+    def dfs(self, root, values):
+        if root is None:
+            return
+        values.append(root.data)
+        self.dfs(root.left, values)
+        self.dfs(root.right, values)
